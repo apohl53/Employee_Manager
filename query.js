483b91f7b1ce7ae1a5c5955314c9388ee5e731b3
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-10-14 21:29:20 -0400

    created the rest of the functions, tried using a query page but moved everything back to server.js after circular dependancy issues

--- query.js
+++ query.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const { askQuestions } = require("./server");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -8,62 +9,6 @@ const connection = mysql.createConnection({
   password: "",
   database: "employee_db",
 });
-
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  askQuestions();
-});
-
-function askQuestions() {
-  inquirer
-    .prompt({
-      message: "what would you like to do?",
-      type: "list",
-      choices: [
-        "view all employees",
-        "view all departments",
-        "add employee",
-        "add department",
-        "add role",
-        "update employee role",
-        "QUIT",
-      ],
-      name: "choice",
-    })
-    .then((answers) => {
-      console.log(answers.choice);
-      switch (answers.choice) {
-        case "view all employees":
-          viewEmployees();
-          break;
-
-        case "view all departments":
-          viewDepartments();
-          break;
-
-        case "add employee":
-          addEmployee();
-          break;
-
-        case "add department":
-          addDepartment();
-          break;
-
-        case "add role":
-          addRole();
-          break;
-
-        case "update employee role":
-          updateEmployeeRole();
-          break;
-
-        default:
-          connection.end();
-          break;
-      }
-    });
-}
 
 function viewEmployees() {
   connection.query("SELECT * FROM employee", function (err, data) {
@@ -195,3 +140,12 @@ function updateEmployeeRole() {
       askQuestions();
     });
 }
+
+module.exports = {
+  viewEmployees,
+  viewDepartments,
+  updateEmployeeRole,
+  addRole,
+  addDepartment,
+  addEmployee,
+};
--- seeds.sql
+++ seeds.sql
@@ -1,26 +1,28 @@
-INSERT INTO department (name) VALUES
+INSERT INTO department (name)
+ VALUES
   ("Sales"),
   ("Marketing"),
   ("Human Resources"),
   ("Finance"),
   ("Executives");
 
-  INSERT INTO employee (first_name, last_name, role_id) VALUES
-  ("Maverick", "Waters", 1),
-  ("Amina", "Davidson", 2),
-  ("Kenzie", "Hoffman", 3),
-  ("Alex", "Pohlman", 4),
-  ("Christian", "Ortiz", 5),
-  ("Ryder", "Stokes", 6),
-  ("Jacob", "Green", 7),
-  ("Mary", "Marler", 8),
-  ("Bobby", "Bobberson", 10),
-  ("Christie", "Christianson", 11),
-  ("Dave", "Davison", 12),
-  ("Randy", "Random", 13);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES
+  ("Maverick", "Waters", 1, NULL),
+  ("Amina", "Davidson", 2, 1),
+  ("Kenzie", "Hoffman", 3, 2),
+  ("Alex", "Pohlman", 4, NULL),
+  ("Christian", "Ortiz", 5, 2),
+  ("Ryder", "Stokes", 6, NULL),
+  ("Jacob", "Green", 7, 3),
+  ("Mary", "Marler", 8, NULL),
+  ("Bobby", "Bobberson", 10, 2),
+  ("Christie", "Christianson", 11, 2),
+  ("Dave", "Davison", 12, 2),
+  ("Randy", "Random", 13, 3);
 
--- Insert data into the role table
-INSERT INTO role (title, salary, department_id) VALUES
+INSERT INTO role (title, salary, department_id)
+VALUES
   ("President", 1200000, 5),
   ("Vice President", 1000000, 5),
   ("Chief Financial Officer", 100000, 4),
